eb12b46e38495c7ba6522e80515ae474196dd5ee
余庆 <yuqing@yuqingdeMacBook-Air-4.local>
2021-07-07 11:38:37 +0800

    v0.1.0

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # CoordinatesConverter
 WGS84 / BD09 / GCJ02  经纬度坐标互转，基于numpy列运算
 
+## 安装
+
+> pip install CoordinatesConverter
+
 ## 用法
 > import CoordinatesConverter  
 BUS_GPS['lon'],BUS_GPS['lat'] = CoordinatesConverter.gcj02towgs84(BUS_GPS['lon'],BUS_GPS['lat'])
---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+ 
+setuptools.setup(
+    name="CoordinatesConverter",
+    version="0.1.0",
+    author="Qing Yu",
+    author_email="qingyu0815@foxmail.com",
+    description="A tool to convert coordinates between WGS84 GCJ02 and BD09",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/ni1o1/CoordinatesConverter",
+    project_urls={
+        "Bug Tracker": "https://github.com/ni1o1/CoordinatesConverter/issues",
+    },
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
